5648af35ea47b5abc27493e75471f9937b47a8e7
samiyokim <samiyokim@gmail.com>
2024-10-23 22:10:07 -0700

    test event

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import { defineConfig } from 'vite';
 // vite.config.js
 export default defineConfig({
     base: '/',
+    build: {
+        rollupOptions: {
+            terserOptions: {
+                compress: {
+                    drop_console: false, // Keep console.log statements
+                },
+            },
+        },
+    },
 })
